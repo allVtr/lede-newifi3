254e9f168f01ee2d3646f92b7be91e878e5b4e7c
allVtr <97818492+allVtr@users.noreply.github.com>
2024-08-03 01:00:32 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,4 +17,4 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-git clone git clone https://github.com/coolsnowwolf/lede.git
+git clone https://github.com/vernesong/OpenClash package/luci-app-openclash
